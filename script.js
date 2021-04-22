4d43dab6ae41b2fe0522ae1e7a77d14b7372fba7
Nika <nabakhtevelin@gmail.com>
2021-04-22 22:17:13 +0400

    Setting up the search

--- script.js
+++ script.js
@@ -17,63 +17,6 @@ let list = document.getElementById("list");
 let headerText = document.getElementById("active-user-text");
 let buttons = document.getElementById("buttons");
 
-function setList(group){
-  clearList();
-  for(let person of group){
-    let item = document.createElement("li");
-    let link = document.createElement("a");
-    link.href = '#';
-    link.innerText = person.name;
-    item.style.color = "white";
-    const text = document.createTextNode(person.name);
-    item.appendChild(link);
-    list.appendChild(item);
-  }
-  
-  
-  switch(group.length){
-    case 0:
-      headerText.style.top = "140px";
-      buttons.style.top = "150px";
-      noResults();
-      break;
-    case 1:
-      headerText.style.top = "140px";
-      buttons.style.top = "150px";
-      break;
-    case 2:
-      headerText.style.top = "160px";
-      buttons.style.top = "170px";
-      break;
-    case 3:
-      headerText.style.top = "185px";
-      buttons.style.top = "195px";
-      break;
-    case 4:
-      headerText.style.top = "210px";
-      buttons.style.top = "220px";
-      break;
-    case 6:
-      headerText.style.top = "250px";
-      buttons.style.top = "260px";
-      break;
-    case 7:
-      headerText.style.top = "275px";
-      buttons.style.top = "285px";
-      break;
-    case 8:
-      headerText.style.top = "297px";
-      buttons.style.top = "307px";
-      break;
-    case 10:
-      headerText.style.top = "345px";
-      buttons.style.top = "355px";
-      break;
-    default:
-      headerText.style.top = "100px";
-      break;
-  }
-}
 
 function clearList(){
   while(list.firstChild) {
@@ -89,18 +32,7 @@ function noResults(){
   headerText.style.top = "100px";
 }
 
-// This needs to be commented out for a while
-// searchInput.addEventListener("input", (event) => {
-//   let value = event.target.value;
-//   if(value && value.length > 0) {
-//     value = value.trim();
-//     setList(users.filter(person => {
-//       return person.name.includes(value);
-//     }));
-//   }else {
-//     clearList();
-//   }
-// });
+
 
 let elements = document.getElementsByClassName("column");
 let container = document.getElementsByClassName("container");
@@ -144,10 +76,43 @@ function loadUsers() {
       console.log(Array.isArray(users));
       console.log(users);
 
+
+
+      function setList(group){
+        clearList();
+        for(let person of group){
+          let item = document.createElement("li");
+          let link = document.createElement("a");
+          link.href = person.html_url;
+          link.innerText = person.login;
+          item.style.color = "white";
+          const text = document.createTextNode(person.login);
+          item.appendChild(link);
+          list.appendChild(item);
+        }
+      }
+
+
+
+      searchInput.addEventListener("input", (event) => {
+        let value = event.target.value;
+        if(value && value.length > 0) {
+          value = value.trim();
+          setList(users.filter(githubUser => {
+            return githubUser.login.includes(value);
+          }));
+        }else {
+          clearList();
+        }
+      });
+
+
+
       let counter = 0;
       for(i in users) {
           output += `
             <div class="column" style="background-color:#aaa;">
+              <p></p>
               <img src=${users[i].avatar_url} class="image" />
               <ul style="list-style-type: none;">
                 <li><a class="gh-username" href=${users[i].html_url} style="text-decoration: none; color: black;" target="_blank">${users[i].login}</a></li>
@@ -166,3 +131,66 @@ function loadUsers() {
   }
   xhr.send();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// switch(group.length){
+//   case 0:
+//     headerText.style.top = "140px";
+//     buttons.style.top = "150px";
+//     noResults();
+//     break;
+
+//   case 1:
+//     headerText.style.top = "140px";
+//     buttons.style.top = "150px";
+//     break;
+
+//   case 2:
+//     headerText.style.top = "160px";
+//     buttons.style.top = "170px";
+//     break;
+
+//   case 3:
+//     headerText.style.top = "185px";
+//     buttons.style.top = "195px";
+//     break;
+
+//   case 4:
+//     headerText.style.top = "210px";
+//     buttons.style.top = "220px";
+//     break;
+
+//   case 6:
+//     headerText.style.top = "250px";
+//     buttons.style.top = "260px";
+//     break;
+
+//   case 7:
+//     headerText.style.top = "275px";
+//     buttons.style.top = "285px";
+//     break;
+
+//   case 8:
+//     headerText.style.top = "297px";
+//     buttons.style.top = "307px";
+//     break;
+
+//   case 10:
+//     headerText.style.top = "345px";
+//     buttons.style.top = "355px";
+//     break;
+
+//   default:
+//     headerText.style.top = "100px";
+//     break;
+// }
